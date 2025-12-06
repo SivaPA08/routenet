ffdeb7a0a02c7067d174a09c3694acc92c0084ef
Siva <sivaprasada2004@gmail.com>
2025-12-06 12:11:35 +0530

    fixed the spring boot config errors

--- main/src/main/java/com/siva/main/MainApplication.java
+++ main/src/main/java/com/siva/main/MainApplication.java
@@ -2,8 +2,9 @@ package com.siva.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class MainApplication {
 
 	public static void main(String[] args) {
